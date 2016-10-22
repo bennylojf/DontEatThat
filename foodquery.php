296b2b8d3ba72727b35fd0c384d0a5a29cfa47ac
Benny <blo2569@gmail.com>
2016-10-22 12:40:48 -0700

    Changed query to only look for two rows

--- foodquery.php
+++ foodquery.php
@@ -4,6 +4,8 @@ $username = "group14";
 $password = "Cpen&321";
 $host = "localhost";
 $dbname = "Group14DB";
+$input1 = 'Apple';
+$input2 = 'orange';
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -12,7 +14,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT Name, Sugar, Salt, Fat FROM Food";
+$sql = "SELECT Name, Sugar, Salt, Fat FROM Food WHERE (Name = '$input1' OR Name = '$input2')";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
