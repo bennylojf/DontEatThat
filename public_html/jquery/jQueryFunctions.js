d876c93b073dec71a41755f2a2174efee6cd40a1
Kenton <kenton.m.95@hotmail.com>
2016-10-23 10:52:54 -0700

    refactored, added jquery scripts to separate file

---
+++ public_html/jquery/jQueryFunctions.js
@@ -0,0 +1,9 @@
+/* Script used to import navbar */
+$(document).ready(function() {
+    $("#header").load("header.html");
+});
+
+/* Script used to import footer */
+$(document).ready(function() {
+    $("#footer").load("footer.html");
+});
