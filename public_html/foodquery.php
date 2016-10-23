438bbd34ac67a6787449eb6a6b048f20d3baa692
Alex Macdonald <alexmac22347@gmail.com>
2016-10-23 15:02:36 -0700

    The query will now find the average of all results which contain the user input string, and return the average sugar, salt and cholesteral. TODO: first, run spell check on the user input TODO: figure out how to deal with spaces in the user input references #10

--- public_html/foodquery.php
+++ public_html/foodquery.php
@@ -21,17 +21,90 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT Shrt_Desc, Sugar_Tot_g, Sodium_mg, Cholestrl_mg FROM Food WHERE (Shrt_Desc = '$input1' OR Shrt_Desc = '$input2')";
-$result = $conn->query($sql);
+if($input1 != '') {
+    $sql1 = "SELECT Shrt_Desc, Sugar_Tot_g, Sodium_mg, Cholestrl_mg FROM Food WHERE Shrt_Desc LIKE '%$input1%'";
+    $result = $conn->query($sql1);
+    
+    if ($result->num_rows > 0) {
+        // output data of each row
+        $numitems = 1;
+        $totalSugar_g = 0;
+        $totalSalt_mg = 0;
+        $totalCholestrl_mg = 0;
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "Food: " . $row["Shrt_Desc"]. ", Sugar: " . $row["Sugar_Tot_g"]. ", Salt: " . $row["Sodium_mg"]. ", Cholesterol: " . $row["Cholestrl_mg"]. "<br>";
+        while($row = $result->fetch_assoc()) {
+            if($row['Sugar_Tot_g'] != null) {
+                $totalSugar_g = $totalSugar_g + $row['Sugar_Tot_g'];
+            }
+            if($row['Sodium_mg'] != null) {
+                $totalSalt_mg = $totalSalt_mg + $row['Sodium_mg'];
+            }
+            if($row['Cholestrl_mg'] != null) {
+                $totalCholestrl_mg = $totalCholestrl_mg + $row['Cholestrl_mg'];
+            }
+
+            $numitems = $numitems + 1;
+        }
+
+        $averageSugar_g = $totalSugar_g / $numitems;
+        $averageSalt_mg = $totalSalt_mg / $numitems;
+        $averageCholestrl_mg = $totalCholestrl_mg / $numitems;
+
+        echo "Food: " . $input1 . ", Sugar: " . $averageSugar_g. ", Salt: " . $averageSalt_mg. ", Cholesterol: " . $averageCholestrl_mg. "<br>";
+    } 
+    
+    else {
+        echo "0 results" . "<br>";
     }
-} else {
-    echo "0 results";
+} 
+
+else {
+    echo "Enter a food <br>";
 }
+
+echo "-----------------------------------<br>";
+
+if($input2 != '') {
+    $sql2 = "SELECT Shrt_Desc, Sugar_Tot_g, Sodium_mg, Cholestrl_mg FROM Food WHERE Shrt_Desc LIKE '%$input2%'";
+    $result = $conn->query($sql2);
+    
+    if ($result->num_rows > 0) {
+        // output data of each row
+        $numitems = 1;
+        $totalSugar_g = 0;
+        $totalSalt_mg = 0;
+        $totalCholestrl_mg = 0;
+
+        while($row = $result->fetch_assoc()) {
+            if($row['Sugar_Tot_g'] != null) {
+                $totalSugar_g = $totalSugar_g + $row['Sugar_Tot_g'];
+            }
+            if($row['Sodium_mg'] != null) {
+                $totalSalt_mg = $totalSalt_mg + $row['Sodium_mg'];
+            }
+            if($row['Cholestrl_mg'] != null) {
+                $totalCholestrl_mg = $totalCholestrl_mg + $row['Cholestrl_mg'];
+            }
+
+            $numitems = $numitems + 1;
+        }
+
+        $averageSugar_g = $totalSugar_g / $numitems;
+        $averageSalt_mg = $totalSalt_mg / $numitems;
+        $averageCholestrl_mg = $totalCholestrl_mg / $numitems;
+
+        echo "Food: " . $input2 . ", Sugar: " . $averageSugar_g. ", Salt: " . $averageSalt_mg. ", Cholesterol: " . $averageCholestrl_mg. "<br>";
+    } 
+    
+    else {
+        echo "0 results" . "<br>";
+    }
+} 
+
+else {
+    echo "Enter a food <br>";
+}
+
 $conn->close();
 ?>
 
--- upload.sh
+++ upload.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-scp -r public_html/ group14@donteatthat.ca:~/public_html/
-=======
 scp -r public_html/ group14@donteatthat.ca:~/
->>>>>>> a4af2501305f6339231d7a71a19035e2e67cd648
