6f2e7139287a7ef6d2c79dd0a916b2bba01c40f8
Benny Lo <blo2569@gmail.com>
2016-10-26 17:47:58 -0700

    Removed phone box

--- public_html/contact.php
+++ public_html/contact.php
@@ -4,7 +4,7 @@
 $from = 'Demo contact form <demo@domain.com>';
 $sendTo = 'Demo contact form <blo2569@gmail.com>';
 $subject = 'New message from contact form';
-$fields = array('name' => 'Name', 'surname' => 'Surname', 'phone' => 'Phone', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in email
+$fields = array('name' => 'Name', 'surname' => 'Surname', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
 $errorMessage = 'There was an error while submitting the form. Please try again later';
 
