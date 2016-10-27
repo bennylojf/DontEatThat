f848cbfb5e4aae9402f1f69bc3bf7c0b9e260915
Benny <blo2569@gmail.com>
2016-10-27 09:55:58 -0700

    Changed receiving email

--- public_html/contact.php
+++ public_html/contact.php
@@ -2,7 +2,7 @@
 
 // configure
 $from = 'Demo contact form <demo@domain.com>';
-$sendTo = 'Demo contact form <blo2569@gmail.com>';
+$sendTo = 'Demo contact form <donteatthat.contact@gmail.com>';
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'surname' => 'Surname', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in email
 $okMessage = 'Contact form successfully submitted. Thank you, I will get back to you soon!';
