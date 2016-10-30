a172a9e6327809e6ee7b18479861f0d6719a065a
Alex Macdonald <alexmac22347@gmail.com>
2016-10-29 19:26:03 -0700

    Removed fat-secret-php .git folder. Also converted result to array in library

--- public_html/foodquery.php
+++ public_html/foodquery.php
@@ -9,19 +9,13 @@ $secret_key = "68a6ee2e74034474a625a5dfdf2546d1";
 
 $searchTerm1 = $_GET['item1'];
 $searchTerm2 = $_GET['item2'];
-//$searchTerm1 = preg_replace('/\s+/', '%20', $searchTerm1);
-//$searchTerm2 = preg_replace('/\s+/', '%20', $searchTerm2);
 
 $client = new \Adcuz\FatSecret\Client($consumer_key, $secret_key);
 $client->SetMaxResults(1);
 
-$queryResponseString = $client->SearchFood($searchTerm1, false, false, 1);
-
-$json = json_encode($queryResponseString);
-
-$info = json_decode($json, true);
+$searchResult = $client->SearchFood($searchTerm1, false, false, 1);
 
 echo "<br>ID: ";
-echo $info["foods"]["food"]["food_id"];
+echo $searchResult["foods"]["food"]["food_id"];
 
 ?>
