fdcb25048d972d68c0ca320c0dd9774e9035734d
Alex Macdonald <alexmac22347@gmail.com>
2016-10-31 11:53:20 -0700

    Fixed bug where certain foods contained no nutritional information. resolves #22

--- public_html/foodquery.php
+++ public_html/foodquery.php
@@ -33,19 +33,40 @@ return array(
 
 
 function getRelevantData($rawFoodData) {
-    return array(
-        "food_id" => $rawFoodData["food"]["food_id"],
-        "food_name" => $rawFoodData["food"]["food_name"],
-        "metric_serving_unit" => $rawFoodData["food"]["servings"]["serving"][0]["metric_serving_unit"],
-        "metric_serving_amount" => $rawFoodData["food"]["servings"]["serving"][0]["metric_serving_amount"],
-        "cholesterol" => $rawFoodData["food"]["servings"]["serving"][0]["cholesterol"], //mg
-        "calories" => $rawFoodData["food"]["servings"]["serving"][0]["calories"], // kcal
-        "fat" => $rawFoodData["food"]["servings"]["serving"][0]["fat"], // grams
-        "protein" => $rawFoodData["food"]["servings"]["serving"][0]["protein"], // grams
-        "sodium" => $rawFoodData["food"]["servings"]["serving"][0]["sodium"], // mg
-        "carbohydrate" => $rawFoodData["food"]["servings"]["serving"][0]["carbohydrate"], // grams
-        "sugar" => $rawFoodData["food"]["servings"]["serving"][0]["sugar"] // grams
-    );
+    // This is the case where the query contains multiple different ways of serving
+    // Eg, for fried chicken, yeild after cooking, bones removed, sliced, with bone, etc
+    if ($rawFoodData["food"]["servings"]["serving"][0] !== null) {
+        return array(
+            "food_id" => $rawFoodData["food"]["food_id"],
+            "food_name" => $rawFoodData["food"]["food_name"],
+            "metric_serving_unit" => $rawFoodData["food"]["servings"]["serving"][0]["metric_serving_unit"],
+            "metric_serving_amount" => $rawFoodData["food"]["servings"]["serving"][0]["metric_serving_amount"],
+            "cholesterol" => $rawFoodData["food"]["servings"]["serving"][0]["cholesterol"], //mg
+            "calories" => $rawFoodData["food"]["servings"]["serving"][0]["calories"], // kcal
+            "fat" => $rawFoodData["food"]["servings"]["serving"][0]["fat"], // grams
+            "protein" => $rawFoodData["food"]["servings"]["serving"][0]["protein"], // grams
+            "sodium" => $rawFoodData["food"]["servings"]["serving"][0]["sodium"], // mg
+            "carbohydrate" => $rawFoodData["food"]["servings"]["serving"][0]["carbohydrate"], // grams
+            "sugar" => $rawFoodData["food"]["servings"]["serving"][0]["sugar"] // grams
+        );
+    } 
+    // In this case, there is only one serving_description
+    else {
+        return array(
+            "food_id" => $rawFoodData["food"]["food_id"],
+            "food_name" => $rawFoodData["food"]["food_name"],
+            "metric_serving_unit" => $rawFoodData["food"]["servings"]["serving"]["metric_serving_unit"],
+            "metric_serving_amount" => $rawFoodData["food"]["servings"]["serving"]["metric_serving_amount"],
+            "cholesterol" => $rawFoodData["food"]["servings"]["serving"]["cholesterol"], //mg
+            "calories" => $rawFoodData["food"]["servings"]["serving"]["calories"], // kcal
+            "fat" => $rawFoodData["food"]["servings"]["serving"]["fat"], // grams
+            "protein" => $rawFoodData["food"]["servings"]["serving"]["protein"], // grams
+            "sodium" => $rawFoodData["food"]["servings"]["serving"]["sodium"], // mg
+            "carbohydrate" => $rawFoodData["food"]["servings"]["serving"]["carbohydrate"], // grams
+            "sugar" => $rawFoodData["food"]["servings"]["serving"]["sugar"] // grams
+        );
+
+    }
 }
 
 ?>
