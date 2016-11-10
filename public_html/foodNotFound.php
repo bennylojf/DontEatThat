c9ea73d1748f55bc06bf14da10cc7ca46c51163b
Alex Macdonald <alexmac22347@gmail.com>
2016-11-09 22:52:01 -0800

    added countdown timer if food is not found, or if food search gets the same food item. Also slightly improved the timer itself

--- public_html/foodNotFound.php
+++ public_html/foodNotFound.php
@@ -22,6 +22,35 @@
             echo "<br>";
             ?>
 
+            <font face="Helvetica"><b>You will be redirected in <p style="display:inline" id="number"></p> Seconds</b>
+
+            <script type="text/javascript">
+                var targetURL="http://donteatthat.ca";
+                var count = 5;
+                countdown(count);
+
+                function countdown(timer) {
+                    //Keeps the interval ID for later clear
+                    var intervalID;
+                    intervalID = setInterval(function () {
+                
+                        display(timer);
+                        timer = timer - 1;
+                
+                        if (timer < 0) {
+                            clearTimeout(intervalID);
+
+                            window.location=targetURL;
+                            return;
+                        }
+                    }, 1000);
+                }
+                
+                //Modifies the countdown display
+                function display(timer) {
+                    document.getElementById("number").innerHTML = timer;
+                }
+                </script>
        </div>
     </div>
 </div>
--- public_html/foodSameResult.php
+++ public_html/foodSameResult.php
@@ -6,7 +6,38 @@
 <div style="padding-left: 5%;padding-right:5%;">
    <div class="container-fluid">
         <div style="padding-top: 30px;" align="center">
-           Sorry, the items you entered were the same. Please enter two different food items.
+           Sorry, the items you entered were the same. Please enter two different food items. <br>
+
+            <font face="Helvetica"><b>You will be redirected in <p style="display:inline" id="number"></p> Seconds</b>
+
+            <script type="text/javascript">
+                var targetURL="http://donteatthat.ca";
+                var count = 5;
+                countdown(count);
+
+                function countdown(timer) {
+                    //Keeps the interval ID for later clear
+                    var intervalID;
+                    display(timer);
+                    intervalID = setInterval(function () {
+                
+                        timer = timer - 1;
+                        display(timer);
+                
+                        if (timer < 0) {
+                            clearTimeout(intervalID);
+
+                            window.location=targetURL;
+                            return;
+                        }
+                    }, 1000);
+                }
+                
+                //Modifies the countdown display
+                function display(timer) {
+                    document.getElementById("number").innerHTML = timer;
+                }
+                </script>
        </div>
     </div>
 </div>
