9744810752c00372ef3a2aec619abb215ceeebd6
Idontknowwhatisgoingonaswell <15adil95@gmail.com>
2016-11-10 01:00:39 -0800

    manage interface and accounUpdate

---
+++ public_html/accountUpdate.php
@@ -0,0 +1,49 @@
+  <?php 
+   session_start();
+    
+$configs = include('../config/config.php');
+
+// Reference: https://www.tutorialspoint.com/php/php_mysql_login.htm
+
+$username = $configs['database_username'];
+$password = $configs['database_password'];
+$host     = $configs['host'];
+$dbname   = $configs['database_name'];
+
+// Create connection
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+// Check connection
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+    echo "Connected successfully"; 
+	
+	
+	$signupname = $_POST['signup-name'];
+	$signupusername = $_POST['signup-username'];
+	$signuppassword = $_POST['signup-password'];
+	$signupcalories = $_POST['signup-calories'];
+	$signupsugar = $_POST['signup-sugar'];
+	$signupsodium = $_POST['signup-sodium'];
+	$signupprotein = $_POST['signup-protein'];
+
+ $sql = " UPDATE Users SET Name = '$signupname', Username = '$signupusername', Password = '$signuppassword', Calories = '$signupcalories', Sugar = '$signupsugar', Sodium = '$signupsodium', Protein = '$signupprotein' WHERE Username = '{$_SESSION['user_username']}' ";
+ 
+if ($conn->query($sql) === TRUE) {
+	  
+	  echo "<h2>Your account has been updated.</h2>";
+	  header("Refresh: 3; url=http://donteatthat.ca");
+	  exit();
+  }
+  else {
+	  echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  
+$conn->close();
+    ?>
+  
+
+
--- public_html/manage_account.php
+++ public_html/manage_account.php
@@ -4,7 +4,7 @@
    $title = "Account Manager";
    include("header.php"); 
 ?>
-<body>
+
  
 	
 	<!-- Reference: http://bootsnipp.com/snippets/DVXQa
@@ -17,16 +17,13 @@
     <!-- Beginning of logo, input fields and buttons -->
     <div align="center" style="padding-top: 5%;">
         <div class="container-fluid">
-			<form class="form-horizontal" action="accountUpdate.php" method="post">
-            <img class="img-responsive title-style" src="res/logo.jpg">          
+			<form class="form-horizontal" action="accountUpdate.php" method="post">        
                 <h2 style="font-size: 2.5vw;">Manage Account</h2>
-                <form class="form-inline" action="results.php">
+                <form class="form-inline" action="accountUpdate.php">
                 	
 					<div style="padding-top: 40px;" class="form-group form-group-lg">
 				     
                   <!-- FIRST NAME -->
-				 
-               
                <div class="form-group form-group-lg">
                   <label class="col-sm-2 control-label">Name</label>
                   <div class="col-sm-4">
@@ -93,21 +90,22 @@
                 <!-- SAVE CHANGES -->
 			   <div id = "container">
 				<button type="submit" id="save-button" class="btn btn-success btn-lg">Save Changes</button>
-				
-				<!-- DELETE -->  
-				 <button type="submit" id="delete-button" class="btn btn-danger btn-lg">Delete</button>
-               </div>
-			   
-			  
+				 </div>
+				  
 			 </div>
             </div>
          </form>
-      </div>
+		</div>
+		
+				<div div align="center" style="padding-top: 5%;">
+		 <!-- DELETE -->  
+				<form action="deleteAccount.php" method="post">
+				
+				 <button type="submit" id="delete-button" class="btn btn-danger btn-lg">Delete</button>
+				</form>
+				</div>
       <!--End of Form-->
      <?php include "footer.php" ?>
-   </body>
-                	
-   
-</body>
+  
 </html>
 
