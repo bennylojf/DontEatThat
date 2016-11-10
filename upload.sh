1c26ef7b7bbb930c47314e58a9904599626cc1a3
Alex Macdonald <alexmac22347@gmail.com>
2016-11-10 12:19:12 -0800

    Updated upload script. There are options to upload the library folder or the js folder

--- upload.sh
+++ upload.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-scp public_html/*.* group14@donteatthat.ca:~/public_html/
+if [ "$1" == --lib ]; then
+    scp -r public_html/lib/ group14@donteatthat.ca:~/public_html/lib/
+elif [ "$1" == --js ]; then
+    scp -r public_html/js/ group14@donteatthat.ca:~/public_html/js/
+else
+    scp public_html/*.* group14@donteatthat.ca:~/public_html/
+fi
