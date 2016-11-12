47f72892cc277b1acea2def77b39ed55c05fb1d2
Idontknowwhatisgoingonaswell <15adil95@gmail.com>
2016-11-11 19:50:51 -0800

    PHP Testing

---
+++ public_html/FoodFinderTest.php
@@ -0,0 +1,31 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class FoodFinderTest extends TestCase
+{
+	public function testGetFoodDatas(){
+		// Arrange
+        $arr = new FoodFinder();
+			
+        // Act
+        $act = $arr->getFoodDatas();
+
+        // Assert
+        $this->assertEquals(2, count($act));
+	}
+	
+	public function testSetNumberOfSearchTerms(){
+		// Arrange
+		$x = new FoodFinder();
+		
+		var $number = 4;
+		
+		// Act
+		$a = $x->setNumberOfSearchTerms($number);
+		
+		// Assert
+		$this -> assertEquals($number, $a);
+	}
+	
+	
+}
