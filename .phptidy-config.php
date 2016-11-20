130254c7792d9ae28a540613daf9fcef4d044062
Alex Macdonald <alexmac22347@gmail.com>
2016-11-20 13:36:12 -0800

    Added phptidy.php. Use this to clean php code.
    Example: './scripts/phptidy.php replace public_html/classes/MyClass.php'
    
    To format all php code at once:
    './scripts/phptidy.php replace'
    
    NOTE: phptidy will NOT format html!
    
    Also moved scripts to a scripts folder

---
+++ .phptidy-config.php
@@ -0,0 +1,15 @@
+<?php
+
+$project_files = array(
+    "public_html/*.php",
+    "public_html/php/*.php",
+    "public_html/classes/*.php"
+);
+
+$indent_char = "    ";
+
+$add_operator_space = true;
+
+$fix_round_bracket_space = true;
+
+?>
--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Run this script from DonEatThat/
+# Don't run it from the scripts/ directory
+
 if [ "$1" == -r ]; then
     scp -r public_html/ group14@donteatthat.ca:~/
 elif [ "$1" == -p ];  then
     scp public_html/*.* group14@donteatthat.ca:~/public_html/
-    scp  public_html/php/* group14@donteatthat.ca:~/public_html/php/
-    scp  public_html/classes/* group14@donteatthat.ca:~/public_html/classes/
+    scp public_html/php/* group14@donteatthat.ca:~/public_html/php/
+    scp public_html/classes/* group14@donteatthat.ca:~/public_html/classes/
 else
     scp public_html/*.* group14@donteatthat.ca:~/public_html/
 fi
