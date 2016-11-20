5138e3fde57c15f10f85b43257bfeef90c4f7bea
Alex Macdonald <alexmac22347@gmail.com>
2016-11-19 21:50:43 -0800

    Small upload script tweak

--- upload.sh
+++ upload.sh
@@ -4,8 +4,8 @@ if [ "$1" == -r ]; then
     scp -r public_html/ group14@donteatthat.ca:~/
 elif [ "$1" == -p ];  then
     scp public_html/*.* group14@donteatthat.ca:~/public_html/
-    scp -r public_html/php/ group14@donteatthat.ca:~/public_html/
-    scp -r public_html/classes/ group14@donteatthat.ca:~/public_html/
+    scp  public_html/php/* group14@donteatthat.ca:~/public_html/php/
+    scp  public_html/classes/* group14@donteatthat.ca:~/public_html/classes/
 else
     scp public_html/*.* group14@donteatthat.ca:~/public_html/
 fi
