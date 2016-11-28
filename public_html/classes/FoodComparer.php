a8a1ddca873ecfdd6735ebb58805e33ba13d94fd
Benny Lo <blo2569@gmail.com>
2016-11-27 21:18:22 -0800

    Changed algorithm to consider more nutrients

--- public_html/classes/FoodComparer.php
+++ public_html/classes/FoodComparer.php
@@ -12,19 +12,21 @@
 namespace vendor\project;
 
 class FoodComparer {
-    const DAILYCALORIES    = 2500; // kcal
-    const DAILYCARBS       = 300; // g
-    const DAILYPROTEIN     = 50; // g
-    const DAILYFAT         = 65; // g
+    // daily recommended intake
+    // approximations only
+    const DAILYCALORIES = 2300; // kcal
+    const DAILYCARBS = 300; // g
+    const DAILYPROTEIN = 50; // g
+    const DAILYFAT = 65; // g
     const DAILYCHOLESTEROL = 300; // mg
-    const DAILYSODIUM      = 2400; // mg
-    const DAILYPOTASSIUM   = 5000; // mg
-    const DAILYFIBER       = 30; // g
-    const DAILYSUGAR       = 30; // g
-    const DAILYVITA        = 100; // percentage
-    const DAILYVITC        = 100; // percentage
-    const DAILYCALCIUM     = 100; // percentage
-    const DAILYIRON        = 100; // percentage
+    const DAILYSODIUM = 2400; // mg
+    const DAILYPOTASSIUM = 5000; // mg
+    const DAILYFIBER = 30; // g
+    const DAILYSUGAR = 30; // g
+    const DAILYVITAMINA = 100; // percentage
+    const DAILYVITAMINC = 100; // percentage
+    const DAILYCALCIUM = 100; // percentage
+    const DAILYIRON = 100; // percentage
 
     var $userCalories;
     var $userSugar;
@@ -32,61 +34,34 @@ class FoodComparer {
     var $userProtein;
     var $userCalcium;
 
-
     /**
+     * These are the valid arguments:
+     * "High" -> Use more of this field
+     * "Low" -> Use less of this field
+     * Any other string -> Based of the amount an average person should eat in a day
      *
-     * @param string $userCalories - Used to determine if the user wants more, less, or a normal amount of calories
-     *                               "High" -> More calories create a higher score
-     *                               "Low" -> More calories create a lower score
-     *                               Any other string -> Calories are based of the amount an average person should eat in a day
-     * @param string $userSugar - Used to determine if the user wants more, less, or a normal amount of sugar
-     *                            "High" -> More sugar create a higher score
-     *                            "Low" -> More sugar create a lower score
-     *                             Any other string -> sugar is based of the amount an average person should eat in a day
-     *
-     * @param string $userSodium - Used to determine if the user wants more, less, or a normal amount of sodium
-     *                            "High" -> More sodium create a higher score
-     *                            "Low" -> More sodium create a lower score
-     *                             Any other string -> sodium is based of the amount an average person should eat in a day
-     *
-     * @param string $userProtein - Used to determine if the user wants more, less, or a normal amount of protein
-     *                            "High" -> More protein create a higher score
-     *                            "Low" -> More protein create a lower score
-     *                             Any other string -> protein is based of the amount an average person should eat in a day
-     *
-     * @param string $userCalcium - Used to determine if the user wants more, less, or a normal amount of calcium
-     *                            "High" -> More calcium create a higher score
-     *                            "Low" -> More calcium create a lower score
-     *                             Any other string -> calcium is based of the amount an average person should eat in a day
-     *
+     * @param string  $userCalories - Used to determine if the user wants more, less, or a normal amount of calories
+     * @param string  $userSugar    - Used to determine if the user wants more, less, or a normal amount of sugar
+     * @param string  $userSodium   - Used to determine if the user wants more, less, or a normal amount of sodium
+     * @param string  $userProtein  - Used to determine if the user wants more, less, or a normal amount of protein
+     * @param string  $userCalcium  - Used to determine if the user wants more, less, or a normal amount of calcium
      */
     function __construct( $userCalories, $userSugar, $userSodium, $userProtein, $userCalcium ) {
         $this->userCalories = $userCalories;
-        $this->userSugar = $userSugar;
-        $this->userSodium = $userSodium;
-        $this->userProtein = $userProtein;
-        $this->userCalcium = $userCalcium;
+        $this->userSugar    = $userSugar;
+        $this->userSodium   = $userSodium;
+        $this->userProtein  = $userProtein;
+        $this->userCalcium  = $userCalcium;
     }
 
 
-    // This method compares multiple food items, and returns an array sorted of food items
-    // sorted by healthiness.
-    //
-    // Input: foodDatas            - Array of different foods to be compared
-    //        $useNormalizeWeights - set this to true if you want to compare
-    //                               100 grams of each item, and disregard the serving size
-    //
-    // Returns:                    - The foodDatas array will be returned, but each element will have a new field call "score"
     /**
-     * The higher the score, the healthier the food
+     * An array with each item in $foodDatas with a 'score' field is returned
+     * The higher the score, the healthier the food.
      *
-     * @param array $foodDatas - An multidimensional array of food. Element should be an array of
-     *                           information for a particular food
-     * @param bool $useNormalizeWeights - Set this to true if you want to compare 100 grams
-     *                                       of each food item. Set to false if you want to compare
-     *                                       based on serving size.
+     * @param array   $foodDatas           - An multidimensional array of food. Element should be an array of
+     * @param bool    $useNormalizeWeights - Set this to true if you want to compare 100 grams
      * @return array - The exact same array as $foodDatas, but each item has a new field called 'score'
-     *                  'score' is a float. The higher the 'score', the healthier the food is.
      */
     function getScores( $foodDatas, $useNormalizeWeights ) {
 
@@ -105,8 +80,13 @@ class FoodComparer {
             $fat         = $foodData['fat']; // bad
             $cholesterol = $foodData['cholesterol']; // bad
             $sodium      = $foodData['sodium']; // bad
+            $potassium   = $foodData['potassium'];
+            $fiber       = $foodData['fiber'];
             $sugar       = $foodData['sugar']; // bad
+            $vitamin_a   = $foodData['vitamin_a'];
+            $vitamin_c   = $foodData['vitamin_c'];
             $calcium     = $foodData['calcium']; // good
+            $iron        = $foodData['iron'];
 
             if ($useNormalizeWeights) {
                 $calories    = $this->normalizeWeight($calories, $foodData['metric_serving_amount']);
@@ -115,8 +95,13 @@ class FoodComparer {
                 $fat         = $this->normalizeWeight($fat, $foodData['metric_serving_amount']);
                 $cholesterol = $this->normalizeWeight($cholesterol, $foodData['metric_serving_amount']);
                 $sodium      = $this->normalizeWeight($sodium, $foodData['metric_serving_amount']);
+                $potassium   = $this->normalizeWeight($potassium, $foodData['metric_serving_amount']);
+                $fiber       = $this->normalizeWeight($fiber, $foodData['metric_serving_amount']);
                 $sugar       = $this->normalizeWeight($sugar, $foodData['metric_serving_amount']);
+                $vitamin_a   = $this->normalizeWeight($vitamin_a, $foodData['metric_serving_amount']);
+                $vitamin_c   = $this->normalizeWeight($vitamin_c, $foodData['metric_serving_amount']);
                 $calcium     = $this->normalizeWeight($calcium, $foodData['metric_serving_amount']);
+                $iron        = $this->normalizeWeight($iron, $foodData['metric_serving_amount']);
             }
 
             $caloriesScale = 1;
@@ -169,21 +154,15 @@ class FoodComparer {
             // However, many foods have an amount of protein
             // which is a large portion of the daily intake,
             // so they overwhelm the algorithm.
-            // When the amount of protein you eat 
+            // When the amount of protein you eat
             // approaches the daily amount, it becomes
             // less healthy. this is reflected in this variable.
             // As the amount of protein approaches the daily amount,
             // this variable approaches 0
             $proteinNormalizer = -(1.0 / FoodComparer::DAILYPROTEIN) * ($protein) + 1.0;
 
-            $score = -($caloriesScale) * ($calories / FoodComparer::DAILYCALORIES)
-                + ($carbs / FoodComparer::DAILYCARBS)
-                + ($proteinNormalizer) * ($proteinScale) * ($protein / FoodComparer::DAILYPROTEIN)
-                - ($fat / FoodComparer::DAILYFAT)
-                - ($cholesterol / FoodComparer::DAILYCHOLESTEROL)
-                - ($sodiumScale) * ($sodium / FoodComparer::DAILYSODIUM)
-                - ($sugarScale) * ($sugar / FoodComparer::DAILYSUGAR)
-                + ($calciumScale) * ($calcium / FoodComparer::DAILYCALCIUM) ;
+            $score = -($caloriesScale) * ($calories / FoodComparer::DAILYCALORIES) - ($fat / FoodComparer::DAILYFAT) - ($cholesterol / FoodComparer::DAILYCHOLESTEROL) - ($sodiumScale) * ($sodium / FoodComparer::DAILYSODIUM) - ($sugarScale) * ($sugar / FoodComparer::DAILYSUGAR) + ($potassium / FoodComparer::DAILYPOTASSIUM) + ($fiber / FoodComparer::DAILYFIBER) + ($vitamin_a / FoodComparer::DAILYVITAMINA) + ($vitamin_c / FoodComparer::DAILYVITAMINC) + ($calcium / FoodComparer::DAILYCALCIUM) + ($iron / FoodComparer::DAILYIRON)
+                +($proteinNormalizer) * ($protein / FoodComparer::DAILYPROTEIN);
 
             $foodData['score'] = $score;
         }
@@ -191,20 +170,14 @@ class FoodComparer {
         return $foodDatas;
     }
 
-
     /**
-     *
-     * @param float $field - The field you want to normalize. This is a number. For example, number
-     *                       of grams of protein, or milligrams of sodium.
-     * @param float $servingSize - The serving size of the food. Eg, a banana weighs 80 grams, so you would put 80 in here.
+     * @param float   $field       - The field you want to normalize. This is a number. For example, number of grams of protein, or milligrams of sodium.
+     * @param float   $servingSize - The serving size of the food. Eg, a banana weighs 80 grams, so you would put 80 in here.
      * @return The amount of $field per 100 grams (assuming serving size is in grams)
      */
     function normalizeWeight($field, $servingSize) {
         return round(($field / $servingSize) * 100);
     }
-
-
 }
-
 
 ?>
--- public_html/classes/FoodFinder.php
+++ public_html/classes/FoodFinder.php
@@ -2,7 +2,7 @@
 /**
  * public_html/classes/FoodFinder.php
  *
- * This is a wrapper class for the FatSecret API. It filters out 
+ * This is a wrapper class for the FatSecret API. It filters out
  * unnecessary information.
  *
  * @package default
@@ -21,36 +21,24 @@ class FoodFinder {
     var $numberOfSearchTerms = 5;
     var $client;
 
-
     /**
-     *
-     * @param string $consumerKey - API consumer key, provided by FatSecret
-     * @param string $secretKey - API secret key, provided by FatSecret
+     * @param string  $consumerKey - API consumer key, provided by FatSecret
+     * @param string  $secretKey   - API secret key, provided by FatSecret
      */
     function __construct( $consumerKey, $secretKey ) {
         $this->client = new Client( $consumerKey, $secretKey );
     }
 
-
     /**
-     *
-     * @param string $searchTerm - The name of the food you want to search for. Eg, 'Apple'
-     * @return array - An array containing various nutritional information about
-     *                 the food item.
+     * @param string  $searchTerm - The name of the food you want to search for. Eg, 'Apple'
+     * @return array - An array containing various nutritional information about the food item.
      */
     function runQuery($searchTerm) {
         // This initial search does not give us enough data. It only contains a
         // few basic facts like ID, sugar, some other stuff I forgot
         $searchResult = $this->client->SearchFood($searchTerm, false, false, $this->numberOfSearchTerms);
-
-        // This can happen if throttling occurs
-        if(isset($searchResult['error']['code'])) {
-            var_dump($searchResult);
-            return $searchResult;
-        }
-
         // If initial search failed, we're done
-        if($searchResult['foods']['total_results'] == 0) {
+        if ($searchResult['foods']['total_results'] == 0) {
             return null;
         }
 
@@ -63,29 +51,24 @@ class FoodFinder {
         // Some items have a strange serving units (like 1 plate or something)
         for ($i = 0; $i < $this->numberOfSearchTerms; $i++) {
             $rawFoodData = $this->client->GetFood($foodIDs[$i]);
-            $foodData = $this->getRelevantData($rawFoodData);
+            $foodData    = $this->getRelevantData($rawFoodData);
             if (isset($foodData)) {
                 break;
             }
         }
 
-
         return $foodData;
     }
 
-
     /**
-     *
-     * @param int $num - The number of items to search through when doing the API call.
-     *                   By default this is 5. After 5 items, the items returned by the API
-     *                   start to become less relevant.
+     * @param int     $num - The number of items to search through when doing the API call. By default this is 5. After 5 items, the items returned by the API start to become less relevant.
      */
     function setNumberOfSearchTerms($num) {
         $this->numberOfSearchTerms = $num;
     }
 
-
-    private function getRelevantData($rawFoodData) { // This is the case where the query contains multiple different ways of serving
+    private function getRelevantData($rawFoodData) // This is the case where the query contains multiple different ways of serving
+        {
 
         // If we went able to find something with a valid serving unit,
         // we set the foodData to null so that whoever uses this knows
@@ -103,14 +86,19 @@ class FoodFinder {
                 "food_id" => $rawFoodData["food"]["food_id"],
                 "food_name" => $rawFoodData["food"]["food_name"],
                 "metric_serving_amount" => $this->convertToGrams($rawFoodData["food"]["servings"]["serving"][0]["metric_serving_amount"], $metric_serving_unit),
-                "cholesterol" => $rawFoodData["food"]["servings"]["serving"][0]["cholesterol"], //mg
                 "calories" => $rawFoodData["food"]["servings"]["serving"][0]["calories"], // kcal
-                "fat" => $rawFoodData["food"]["servings"]["serving"][0]["fat"], // grams
-                "protein" => $rawFoodData["food"]["servings"]["serving"][0]["protein"], // grams
-                "sodium" => $rawFoodData["food"]["servings"]["serving"][0]["sodium"], // mg
                 "carbohydrate" => $rawFoodData["food"]["servings"]["serving"][0]["carbohydrate"], // grams
+                "protein" => $rawFoodData["food"]["servings"]["serving"][0]["protein"], // grams
+                "fat" => $rawFoodData["food"]["servings"]["serving"][0]["fat"], // grams
+                "cholesterol" => $rawFoodData["food"]["servings"]["serving"][0]["cholesterol"], //mg
+                "sodium" => $rawFoodData["food"]["servings"]["serving"][0]["sodium"], // mg
+                "potassium" => $rawFoodData["food"]["servings"]["serving"][0]["potassium"], // mg
+                "fiber" => $rawFoodData["food"]["servings"]["serving"][0]["fiber"], // mg
                 "sugar" => $rawFoodData["food"]["servings"]["serving"][0]["sugar"], // grams
-                "calcium" => $rawFoodData["food"]["servings"]["serving"][0]["calcium"] // ???
+                "vitamin_a" => $rawFoodData["food"]["servings"]["serving"][0]["vitamin_a"], // percentage of daily recommended value
+                "vitamin_c" => $rawFoodData["food"]["servings"]["serving"][0]["vitamin_c"], // percentage of daily recommended value
+                "calcium" => $rawFoodData["food"]["servings"]["serving"][0]["calcium"], // percentage of daily recommended value
+                "iron" => $rawFoodData["food"]["servings"]["serving"][0]["iron"] // percentage of daily recommended value
             );
         }
 
@@ -128,18 +116,22 @@ class FoodFinder {
                 "food_id" => $rawFoodData["food"]["food_id"],
                 "food_name" => $rawFoodData["food"]["food_name"],
                 "metric_serving_amount" => $this->convertToGrams($rawFoodData["food"]["servings"]["serving"]["metric_serving_amount"], $metric_serving_unit),
-                "cholesterol" => $rawFoodData["food"]["servings"]["serving"]["cholesterol"], //mg
                 "calories" => $rawFoodData["food"]["servings"]["serving"]["calories"], // kcal
-                "fat" => $rawFoodData["food"]["servings"]["serving"]["fat"], // grams
-                "protein" => $rawFoodData["food"]["servings"]["serving"]["protein"], // grams
-                "sodium" => $rawFoodData["food"]["servings"]["serving"]["sodium"], // mg
                 "carbohydrate" => $rawFoodData["food"]["servings"]["serving"]["carbohydrate"], // grams
+                "protein" => $rawFoodData["food"]["servings"]["serving"]["protein"], // grams
+                "fat" => $rawFoodData["food"]["servings"]["serving"]["fat"], // grams
+                "cholesterol" => $rawFoodData["food"]["servings"]["serving"]["cholesterol"], //mg
+                "sodium" => $rawFoodData["food"]["servings"]["serving"]["sodium"], // mg
+                "potassium" => $rawFoodData["food"]["servings"]["serving"]["potassium"], // mg
+                "fiber" => $rawFoodData["food"]["servings"]["serving"]["fiber"], // mg
                 "sugar" => $rawFoodData["food"]["servings"]["serving"]["sugar"], // grams
-                "calcium" => $rawFoodData["food"]["servings"]["serving"]["calcium"] // ???
+                "vitamin_a" => $rawFoodData["food"]["servings"]["serving"]["vitamin_a"], // percentage of daily recommended value
+                "vitamin_c" => $rawFoodData["food"]["servings"]["serving"]["vitamin_c"], // percentage of daily recommended value
+                "calcium" => $rawFoodData["food"]["servings"]["serving"]["calcium"], // percentage of daily recommended value
+                "iron" => $rawFoodData["food"]["servings"]["serving"]["iron"] // percentage of daily recommended value
             );
         }
     }
-
 
     private function convertToGrams($amount, $servingUnit) {
         if ($servingUnit == "g") {
@@ -148,9 +140,6 @@ class FoodFinder {
             return $amount * 28.35;
         }
     }
-
-
 }
-
 
 ?>
