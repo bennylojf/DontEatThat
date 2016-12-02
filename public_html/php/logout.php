283734ece64d5dd1673506792bfc8773be800057
Alex Macdonald <alexmac22347@gmail.com>
2016-12-01 19:18:50 -0800

    logout.php will now also clear the session variable

--- public_html/php/logout.php
+++ public_html/php/logout.php
@@ -3,5 +3,6 @@ if(session_status() == PHP_SESSION_NONE) {
     session_start();
 }
 session_destroy(); // "logout"
+$_SESSION = array(); // this actually clears the session variable
 header("Location: ../index.php");
 ?>
